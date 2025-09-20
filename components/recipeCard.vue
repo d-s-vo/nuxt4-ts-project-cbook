ad995d42c2922cb05109dfa1c1d3276f5d301f19
Dmitriy Smirnov <smirnov@whyme.agency>
2025-09-20 14:51:44 +0300

    в карточку рецепта добавил метод для перехода на внутреннюю страницу рецепта

--- components/recipeCard.vue
+++ components/recipeCard.vue
@@ -1,17 +1,19 @@
 <template>
-    <div 
-        class="flex gap-[20px] [@media(min-width: 768px)]:flex-col"
+    <div
+        v-if="recipe"
+        @click.prevent="navigateToRecipe(recipe.id)"
+        class="cursor-pointer flex gap-[20px] group"
     >
         <img
             v-if="recipe.imageUrl" 
             :src="recipe.imageUrl" 
-            alt="recipe.title"
+            :alt="recipe.title"
             class="max-w-[100px] aspect-square shrink-0 rounded-[10px]"
         >
         <div>
             <h2
                 v-if="recipe?.title"
-                class="text-[24px] font-bold"
+                class="text-[24px] font-bold group-hover:underline"
             >
                 {{ recipe.title }}
             </h2>
@@ -22,8 +24,15 @@
 </template>
 <script setup lang="ts">
 import type { Recipe } from '~/types';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
 
 defineProps<{
     recipe: Recipe;
 }>();
+
+const navigateToRecipe = (id: number) => {
+    router.push(`/recipe-page?id=${id}`);
+};
 </script>
