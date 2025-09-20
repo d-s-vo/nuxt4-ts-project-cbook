d7f036181631d26123c95d526d80fc99a8cff76f
Dmitriy Smirnov <smirnov@whyme.agency>
2025-09-20 13:23:20 +0300

    добавил оформление дял карточки рецепта на главной странице

--- components/recipeCard.vue
+++ components/recipeCard.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-       {{ recipe }} 
+    <div 
+        class="flex gap-[20px] [@media(min-width: 768px)]:flex-col"
+    >
+        <img
+            v-if="recipe.imageUrl" 
+            :src="recipe.imageUrl" 
+            alt="recipe.title"
+            class="max-w-[100px] aspect-square shrink-0 rounded-[10px]"
+        >
+        <div>
+            <h2
+                v-if="recipe?.title"
+                class="text-[24px] font-bold"
+            >
+                {{ recipe.title }}
+            </h2>
+            <div>Время приготовления: {{ recipe.cookingTime }} минут</div>
+            <div>Сложность: {{ recipe.difficulty }}</div>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
