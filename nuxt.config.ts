d28d2725b13381dc74d3a03397aa90fd309410f8
Dmitriy <smirnov@whyme.agency>
2026-06-25 11:03:24 +0300

    feat: подключение фавиконок

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,7 +42,28 @@ export default defineNuxtConfig({
   css: ['./app/assets/css/main.css'],
   modules: ['@nuxt/ui', '@nuxtjs/device'],
   app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
+    pageTransition: { name: 'page', mode: 'out-in' },
+    head: {
+			viewport: 'width=device-width, initial-scale=1, minimum-scale=1.0',
+			link: [
+				// favicons
+				{ rel: 'icon', type: 'image/png', sizes: '96x96', href: '/favicon-96x96.png' },
+				{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+				{ rel: 'shortcut icon', href: '/favicon.ico' },
+				{ rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+				{ rel: 'manifest', href: '/site.webmanifest' }
+			],
+			htmlAttrs: {
+				lang: 'ru'
+			},
+			
+			meta: [
+				// favicons
+				{ name: 'msapplication-TileColor', content: '#ffffff' },
+				{ name: 'theme-color', content: '#ffffff' },
+				{ name: 'apple-mobile-web-app-title', content: 'CookBook' }
+			]
+		}
   },
   plugins: [
     '~/plugins/bvi-speech-tracker.client.ts'
